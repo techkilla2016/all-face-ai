62b45a6ceb1828d9c550c03ffe2858ef8b4cb8cc
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-10-19 17:57:37 +0530

    swap face

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import './style.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
---
+++ src/data/index.js
@@ -0,0 +1,59 @@
+const ImgData = [
+    {
+        img: '/1.jpg',
+        base64: ''
+    },
+    {
+        img: '/2.png',
+        base64: ''
+    },
+    {
+        img: '/3.jpg',
+        base64: ''
+    },
+    {
+        img: '/4.jpg',
+        base64: ''
+    },
+    {
+        img: '/5.png',
+        base64: ''
+    },
+    {
+        img: '/6.jpg',
+        base64: ''
+    },
+    {
+        img: '/7.jpg',
+        base64: ''
+    },
+    {
+        img: '/8.jpg',
+        base64: ''
+    },
+    {
+        img: '/9.jpeg',
+        base64: ''
+    },
+    {
+        img: '/10.jpg',
+        base64: ''
+    },
+    {
+        img: '/11.jpg',
+        base64: ''
+    },
+    {
+        img: '/12.jpg',
+        base64: ''
+    },
+    {
+        img: '/13.jpg',
+        base64: ''
+    },
+    {
+        img: '/14.jpg',
+        base64: ''
+    }
+]
+export default ImgData
